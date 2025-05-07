98ffd766d74095f2aa7af25ea990f80cb04e83c4
furkaltin <furkanaltin1@gmail.com>
2025-05-07 23:57:58 +0300

    Initial Commit

---
+++ MediaForge/MediaForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaForgeApp.swift
+//  MediaForge
+//
+//  Created by Selin Çağlar on 7.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MediaForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MediaForgeTests/MediaForgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MediaForgeTests.swift
+//  MediaForgeTests
+//
+//  Created by Selin Çağlar on 7.05.2025.
+//
+
+import Testing
+@testable import MediaForge
+
+struct MediaForgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
